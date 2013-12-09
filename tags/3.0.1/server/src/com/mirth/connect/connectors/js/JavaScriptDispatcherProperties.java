aa2cd42da3bdc90c34f3de9b4ff167ba23abf656
jacobb <jacobb@c801cdd1-710f-0410-a44a-8d9edceeb29b>
2013-12-09 21:24:02 +0000

    Tagged 3.0.1

---
+++ tags/3.0.1/server/src/com/mirth/connect/connectors/js/JavaScriptDispatcherProperties.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) Mirth Corporation. All rights reserved.
+ * 
+ * http://www.mirthcorp.com
+ * 
+ * The software in this package is published under the terms of the MPL license a copy of which has
+ * been included with this distribution in the LICENSE.txt file.
+ */
+
+package com.mirth.connect.connectors.js;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+
+import com.mirth.connect.donkey.model.channel.ConnectorProperties;
+import com.mirth.connect.donkey.model.channel.DispatcherConnectorPropertiesInterface;
+import com.mirth.connect.donkey.model.channel.QueueConnectorProperties;
+import com.mirth.connect.donkey.util.DonkeyElement;
+
+public class JavaScriptDispatcherProperties extends ConnectorProperties implements DispatcherConnectorPropertiesInterface {
+    public static final String NAME = "JavaScript Writer";
+
+    private QueueConnectorProperties queueConnectorProperties;
+    private String script;
+
+    public JavaScriptDispatcherProperties() {
+        queueConnectorProperties = new QueueConnectorProperties();
+
+        script = "";
+    }
+    
+    public JavaScriptDispatcherProperties(JavaScriptDispatcherProperties props) {
+        queueConnectorProperties = new QueueConnectorProperties(props.getQueueConnectorProperties());
+
+        script = props.getScript();
+    }
+
+    @Override
+    public String getProtocol() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    public String getScript() {
+        return script;
+    }
+
+    public void setScript(String script) {
+        this.script = script;
+    }
+
+    @Override
+    public String toFormattedString() {
+        return "Script Executed";
+    }
+
+    @Override
+    public QueueConnectorProperties getQueueConnectorProperties() {
+        return queueConnectorProperties;
+    }
+
+    @Override
+    public ConnectorProperties clone() {
+        return new JavaScriptDispatcherProperties(this);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public void migrate3_0_1(DonkeyElement element) {}
+}
